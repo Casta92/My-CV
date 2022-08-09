8919636a2e40e67723be6cd1ef5669531db3eda4
Casta92 <afcastanedam@gmail.com>
2022-08-09 07:55:11 -0500

    Arreglo boton chistes

--- JS/Contac.js
+++ JS/Contac.js
@@ -6,18 +6,19 @@ boton.addEventListener("click", chistesAleatorios)
 
 function chistesAleatorios (){
 
-const endpoint="https://api.dadjokes.io/api/random/joke";
-async function apiAsync(){
-    const response = await fetch(endpoint);
-    const data = await response.json()
-    console.log(data)
-    const ListaChistes = data.body;
-        const primerChiste = ListaChistes[0];
-        const chiste = primerChiste.setup;
-        const respuesta = primerChiste.punchline;
-        document.getElementById("texto-inicial").innerHTML = chiste;
-        document.getElementById("texto-respuesta").innerHTML = respuesta;
-        console.log(chiste, respuesta); 
-}
+    const endpoint="https://api.dadjokes.io/api/random/joke";
+    async function apiAsync()
+        {
+            const response = await fetch(endpoint);
+            const data = await response.json()
+            console.log(data)
+            const ListaChistes = data.body;
+            const primerChiste = ListaChistes[0];
+            const chiste = primerChiste.setup;
+            const respuesta = primerChiste.punchline;
+            document.getElementById("texto-inicial").innerHTML = chiste;
+            document.getElementById("texto-respuesta").innerHTML = respuesta;
+            console.log(chiste, respuesta); 
+        }
     apiAsync()
 }
