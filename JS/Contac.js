13629fc0e5d0613e3cfaca7f5f3fdfeea46352ab
Casta92 <afcastanedam@gmail.com>
2022-08-16 11:03:11 -0500

    Ajuste de código JS

--- JS/Contac.js
+++ JS/Contac.js
@@ -1,24 +1,30 @@
+// window.addEventListener('DOMContentLoaded', chistesAleatorios)
+
+
 let chisteParteI = document.getElementById("texto-inicial");
 let chisteParteII = document.getElementById("texto-respuesta");
 let boton = document.getElementById("Uno-mas");
 
-boton.addEventListener("click", chistesAleatorios)
 
-function chistesAleatorios (){
+boton.addEventListener("click", ()=>chistesAleatorios())
 
-    const endpoint="https://api.dadjokes.io/api/random/joke";
-    async function apiAsync()
-        {
-            const response = await fetch(endpoint);
-            const data = await response.json()
-            console.log(data)
-            const ListaChistes = data.body;
-            const primerChiste = ListaChistes[0];
-            const chiste = primerChiste.setup;
-            const respuesta = primerChiste.punchline;
-            document.getElementById("texto-inicial").innerHTML = chiste;
-            document.getElementById("texto-respuesta").innerHTML = respuesta;
-            console.log(chiste, respuesta); 
-        }
-    apiAsync()
+async function chistesAleatorios() {
+
+
+    const endpoint = "https://api.dadjokes.io/api/random/joke";
+    let response;
+    try {
+        response = await fetch(endpoint);
+    } catch (error) {
+        console.log('ups!!')
+    }
+    const data = await response.json();
+    console.log(data)
+    const ListaChistes = data.body;
+    const primerChiste = ListaChistes[0];
+    const chiste = primerChiste.setup;
+    const respuesta = primerChiste.punchline;
+    document.getElementById("texto-inicial").textContent = chiste;
+    document.getElementById("texto-respuesta").textContent = respuesta;
+    console.log(chiste, respuesta);
 }
